701eec4ac86914ceb7fc06c35fc07040a4c265d0
Emil Kivelä <emil.kivela@helsinki.fi>
2021-04-10 16:46:45 +0300

    added .py files

---
+++ routes.py
@@ -0,0 +1,13 @@
+from flask import render_template, redirect, request
+from app import app
+import chats, users, messages
+
+@app.route("/")
+def index():
+    dfsdfs
+    return render_template("index.html", 
+@app.route()
+
+@app.route()
+
+@app.route()
