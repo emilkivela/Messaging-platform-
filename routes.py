71fb391391da72389ae7f5bdd81df2d97163578b
Emil Kivelä <emil.kivela@helsinki.fi>
2021-04-11 20:27:20 +0300

    lisätty login()-testi

--- routes.py
+++ routes.py
@@ -5,3 +5,17 @@ import chats, users, messages
 @app.route("/")
 def index():
     return render_template("index.html") 
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "GET":
+        username = request.form["username"]
+        password = request.form["password"]
+        session["username"] = username
+        return redirect("/")
+
+@app.route("/logout")
+def logout ():
+    del session["username"]
+    return redirect("/")
+
